7d92b59362898af89d2d000965ca7a15ee7bce53
Valdonix2 <123013224+Valdonix2@users.noreply.github.com>
2023-03-27 21:18:03 +0300

    Update CapybaraHub.lua

--- CapybaraHub.lua
+++ CapybaraHub.lua
@@ -1,1 +1,18 @@
-
+if game.PlaceId == 8737602449 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Valdonix2/EpicGamingMoment/main/PlsDonate.lua'))()
+  end
+else if game.PlaceId == 4639625707 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Vortex194/main/main/oilwarfare", true))()
+  end
+else if game.PlaceId == 6735572261 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/vep1032/VepStuff/main/Pilgrammed%20Free"))()
+  end
+else if game.PlaceId == 9872472334 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Evade/main.lua"))()
+  end
+else if game.PlaceId == 10118559731 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/elonof/nicosbots-script/main/main.lua",true))()
+  end
+else if game.PlaceId == 263761432 then
+  loadstring(game:HttpGet("https://paste.ee/r/GzZjj", true))()
+  end
